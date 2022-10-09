627fc77b418c5fa71855602569d000cc827e107e
inesjpedro <inesjpedro@ist.utl.pt>
2022-10-09 18:45:37 +0100

    Run black for formatting code

--- src/app_gradio/app.py
+++ src/app_gradio/app.py
@@ -1,15 +1,14 @@
 import os
+import re
 from pathlib import Path
-from typing import Callable, List
+from typing import Callable
 
 import gradio as gr
+import torch
 from PIL.Image import Image
 from gradio import CSVLogger
-
-import torch
-import re
-
 from transformers import DonutProcessor, VisionEncoderDecoderModel
+
 
 def main():
     model_inference = ModelInference()
@@ -28,12 +27,11 @@ def make_frontend(fn: Callable[[Image], str]):
     ]
 
     with gr.Blocks() as frontend:
-        #
         # TODO: Add example images
         # TODO: Change layout https://gradio.app/controlling_layout/
         image = gr.Image(type="pil", label="Bookshelf")
         run_button = gr.Button("Find books")
-        #gr.Examples(examples=example_images, inputs=[gr.Image()])
+        # gr.Examples(examples=example_images, inputs=[gr.Image()])
         output = gr.Textbox(label="Recognized books")
         wrong_prediction_button = gr.Button("Flag wrong prediction 🐞")
         user_feedback = gr.Textbox(interactive=True, label="User feedback")
@@ -78,42 +76,52 @@ class ModelInference:
     a dummy class that returns the same dummy prediction irrespectively
     of the input.
     """
+
     def __init__(self):
         """Initializes processing and inference models."""
         self.processor = DonutProcessor.from_pretrained("jay-jojo-cheng/donut-cover")
-        self.model = VisionEncoderDecoderModel.from_pretrained("jay-jojo-cheng/donut-cover")
+        self.model = VisionEncoderDecoderModel.from_pretrained(
+            "jay-jojo-cheng/donut-cover"
+        )
 
     def predict(self, image):
-        
+
         pixel_values = self.processor(image, return_tensors="pt").pixel_values
-    
+
         task_prompt = "<s_cord-v2>"
         decoder_input_ids = self.processor.tokenizer(
-            task_prompt, 
-            add_special_tokens=False, 
-            return_tensors="pt")["input_ids"]
+            task_prompt, add_special_tokens=False, return_tensors="pt"
+        )["input_ids"]
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model.to(device)
 
-        outputs = self.model.generate(pixel_values.to(device),
-                                    decoder_input_ids=decoder_input_ids.to(device),
-                                    max_length=self.model.decoder.config.max_position_embeddings,
-                                    early_stopping=True,
-                                    pad_token_id=self.processor.tokenizer.pad_token_id,
-                                    eos_token_id=self.processor.tokenizer.eos_token_id,
-                                    use_cache=True,
-                                    num_beams=1,
-                                    bad_words_ids=[[self.processor.tokenizer.unk_token_id]],
-                                    return_dict_in_generate=True,
-                                    output_scores=True,)
+        outputs = self.model.generate(
+            pixel_values.to(device),
+            decoder_input_ids=decoder_input_ids.to(device),
+            max_length=self.model.decoder.config.max_position_embeddings,
+            early_stopping=True,
+            pad_token_id=self.processor.tokenizer.pad_token_id,
+            eos_token_id=self.processor.tokenizer.eos_token_id,
+            use_cache=True,
+            num_beams=1,
+            bad_words_ids=[[self.processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True,
+            output_scores=True,
+        )
         return self._post_process_output(outputs)
 
     def _post_process_output(self, outputs) -> str:
         sequence = self.processor.batch_decode(outputs.sequences)[0]
-        sequence = sequence.replace(self.processor.tokenizer.eos_token, "").replace(self.processor.tokenizer.pad_token, "")
-        sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+        sequence = sequence.replace(self.processor.tokenizer.eos_token, "").replace(
+            self.processor.tokenizer.pad_token, ""
+        )
+        sequence = re.sub(
+            r"<.*?>", "", sequence, count=1
+        ).strip()  # remove first task start token
+        print(f"Prediction: {sequence}")
         return sequence
+
 
 def get_project_root():
     """Returns the path to the project's root directory: deep-tsundoku"""
